47b72608172fcd7ccc7d25d32e048da2d18bb5ac
Benjamin Vernoux <bvernoux@gmail.com>
2025-07-06 18:12:13 +0200

    Update make_windows_package.ps1

--- make_windows_package.ps1
+++ make_windows_package.ps1
@@ -34,6 +34,7 @@ cp $build_dir/source_modules/hermes_source/Release/hermes_source.dll sdrpp_windo
 
 cp $build_dir/source_modules/hydrasdr_rfone_source/Release/hydrasdr_rfone_source.dll sdrpp_windows_x64/modules/
 cp 'C:/Program Files/PothosSDR/bin/hydrasdr.dll' sdrpp_windows_x64/
+cp 'C:/Program Files/PothosSDR/bin/pthreadVC3.dll' sdrpp_windows_x64/
 
 cp $build_dir/source_modules/limesdr_source/Release/limesdr_source.dll sdrpp_windows_x64/modules/
 cp 'C:/Program Files/PothosSDR/bin/LimeSuite.dll' sdrpp_windows_x64/
